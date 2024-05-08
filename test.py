6ca2ec9bfe5e96b2f8410b76c0b97e148279a9aa
BogdoKhan <dnw.dch1@gmail.com>
2024-05-08 16:24:17 +0700

    Added DAQ widget with acquisition and single frame time settings. This data is now transported through SCPI into boards. Added number of entries processed into Spectroscope widget

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ from PyQt5.QtWidgets import (
     QApplication, QMainWindow, 
     QLabel,QPushButton, QWidget, 
     QVBoxLayout, QHBoxLayout, 
-    QTabWidget, QToolBar, QCheckBox
+    QTabWidget, QToolBar, QCheckBox, QTableWidgetItem, QGridLayout, QLineEdit
 )
 from PyQt5.QtCore import (
     pyqtSignal, QThread, 
@@ -39,6 +39,13 @@ import numpy as np
 class ConnectionWidget(QWidget):
     def __init__(self):
         super().__init__()
+        self.setupUI()
+        self.chk_use_master.stateChanged.connect(lambda: self.show_connect())
+
+        self.tbl_devices_list.cellClicked.connect(self.onClicked)
+        #if "Connect" button is pressed, content of devices list is updated in major widget
+
+    def setupUI(self):
         self.conn_window = DeviceConn_MasterSlave()
         self.tbl_devices_list = DevicesMap()        #contains info about devices
         self.chk_use_master = QCheckBox("Use master-slave configuration")
@@ -48,36 +55,33 @@ class ConnectionWidget(QWidget):
 
         layout = QVBoxLayout()
         layout.addWidget(self.chk_use_master)
-        self.chk_use_master.stateChanged.connect(lambda: self.show_connect())
         layout.addWidget(self.conn_window)
         layout.addWidget(self.tbl_devices_list)
         layout.addWidget(self.btn_get_spectrum)
-        layout.addStretch()
+        #layout.addStretch()
 
-        hlayout = QHBoxLayout()
-        hlayout.addLayout(layout)
         #cont = QGroupBox(self)
-        self.setLayout(hlayout)
-        self.setMinimumSize(450,400)
-
-        #if "Connect" button is pressed, content of devices list is updated in major widget
-
-
-
-#change type of connection widget if checkbox is checked, currently is a placeholder
+        self.setLayout(layout)
+        self.setMinimumHeight(300)
+        self.setFixedWidth(450)
+        
+    #change type of connection widget if checkbox is checked, currently is a placeholder
     def show_connect(self):
         if self.chk_use_master.isChecked():
             print("Will use master-slave configuration")
         else:
             print("Will not use master-slave configuration")
-#add data in table for new device
-    def addFullData_on_Connected(self, isConnected, uptime):
+    #add data in table for new device
+    def setupData_onConnected(self, isConnected, uptime, devicesMap, exists):
         props = Device()
-        props.setIP(self.conn_window.lne_ip_edit.text())
-        props.setRole(self.conn_window.cmb_role.currentText())
-        props.setStatus('Undefined')
-        props.setUptime(0.0)
-
+        if exists:
+            props = devicesMap[self.conn_window.lne_ip_edit.text()]
+            props.setRole(self.conn_window.cmb_role.currentText())
+        else:
+            props.setIP(self.conn_window.lne_ip_edit.text())
+            props.setRole(self.conn_window.cmb_role.currentText())
+            props.setStatus('Undefined')
+            props.setUptime(0.0)
         if isConnected:
             props.setStatus('Connected')
             props.setUptime(uptime)
@@ -86,33 +90,99 @@ class ConnectionWidget(QWidget):
             self.conn_window.lne_ip_edit.text(), 
             props
         )
-#for existing device, update info
-    def updFullData_on_Connected(self, isConnected, uptime, devicesMap):
-        props = devicesMap[self.conn_window.lne_ip_edit.text()]
-        props.setRole(self.conn_window.cmb_role.currentText())
+    @pyqtSlot(int, int)
+    def onClicked(self, row, col):
+        deviceIP = self.tbl_devices_list.cellWidget(row, 0).text()
+        self.conn_window.lne_ip_edit.setText(deviceIP)
 
-        if isConnected:
-            props.setStatus('Connected')
-            props.setUptime(uptime)
-        #refresh table
-        self.tbl_devices_list.addData(
-            self.conn_window.lne_ip_edit.text(), 
-            props
-        )
+class DaqWidget(QWidget):
+    def __init__(self, devicesMap):
+        super().__init__()
+        self.nFrames = 0
+
+        self.setupUI()
+        self.lne_single_daq_time.textChanged.connect(self.recalcTicks)
+        self.lne_full_daq_time.textChanged.connect(self.recalcFrames)
+        self.btn_updAcqTime.clicked.connect(self.setAcqTime)
+        self.devicesMap = devicesMap
+    
+    def setupUI(self):
+        layout = QGridLayout()
+        self.lne_single_daq_time = QLineEdit()
+        self.lne_single_daq_time.setText("1000")
+        self.lbl_single_daq_ticks = QLabel("0")
+        self.lbl_single_daq_ticks.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        self.lbl_single_daq_time = QLabel("Single DAQ frame time, ms:")
+        self.lbl_ticks = QLabel("ticks")
+
+        self.lne_full_daq_time = QLineEdit("100")
+        self.lbl_full_daq_time = QLabel("Data acquisition time, sec:")
+        self.lbl_full_daq_frames = QLabel("frames")
+        self.lbl_full_daq_frames.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+
+        self.lbl_frames = QLabel("frames")
+
+        self.btn_updAcqTime = QPushButton("Send acq times")
+        self.btn_get_single_spectrum = QPushButton("Get single spectrum")
+        self.btn_get_full_spectrum = QPushButton("Start data acquisition")
+
+        layout.addWidget(self.lbl_single_daq_time, 0, 0, 1, 1)
+        layout.addWidget(self.lne_single_daq_time, 0, 1, 1, 1)
+        layout.addWidget(self.lbl_single_daq_ticks, 0, 2, 1, 1)
+        layout.addWidget(self.lbl_ticks, 0, 3, 1, 1)
+
+        layout.addWidget(self.lbl_full_daq_time, 1, 0, 1, 1)
+        layout.addWidget(self.lne_full_daq_time, 1, 1, 1, 1)
+        layout.addWidget(self.lbl_full_daq_frames, 1, 2, 1, 1)
+        layout.addWidget(self.lbl_frames, 1, 3, 1, 1)
+
+        layout.addWidget(self.btn_updAcqTime, 2, 0, 1, 1)
+        layout.addWidget(self.btn_get_single_spectrum, 2, 1, 1, 1)
+        layout.addWidget(self.btn_get_full_spectrum, 2, 2, 1, 2)
+        self.recalcTicks()
+        self.recalcFrames()
+
+        self.setLayout(layout)
+        self.setMinimumHeight(150)
+        self.setFixedWidth(450)
+
+    def setAcqTime(self):
+        if(len(self.devicesMap.values()) > 0):
+            for device in self.devicesMap.values():
+                device.board.transport.client.write('*IDN?')
+                print("{0}".format(device.board.transport.client.read_raw().decode('utf-8').rstrip()))
+                for chan in device.channels:
+                    query = "ACQusition{0}:TIMEout {1}".format(chan.number, self.lbl_single_daq_ticks.text())
+                    device.board.transport.client.write(query)
+        else:
+            print("Devices map is empty")
+
+
+    def recalcTicks(self):
+        time = int(float(self.lne_single_daq_time.text())/(8e-6))
+        self.lbl_single_daq_ticks.setText("{0}".format(str(time)))
+        self.recalcFrames()
+
+    def recalcFrames(self):
+        self.nFrames = int(float(self.lne_full_daq_time.text())*1000/float(self.lne_single_daq_time.text()))
+        self.lbl_full_daq_frames.setText(str(self.nFrames))
+
+
 
 class MainWindow(QMainWindow):
     signal_dump_wave     = pyqtSignal(object, object, object)
-    signal_dump_spec     = pyqtSignal(object, object, object)
 
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.setupUI()
 
         self.transport_param = {'comname':'', 'speed':'', 'stopbits':'', 'ip':'', 'user':'', 'password':'', 'transport':2, 'port':5000}
         self.daq_cont = {'runned':False, 'need_reset':True, 'dumping_wave':False}
         self.settings = {'ip':'192.168.0.20', 'port':5000}
 
         self.board_thread = QThread()
+        self.thr_daq = QThread()
+
         self.board = api.api(self.daq_cont, self.transport_param, False, False)
         self.board.moveToThread(self.board_thread)
 
@@ -126,16 +196,27 @@ class MainWindow(QMainWindow):
 
         self.board_thread.start() 
 
-        self.thr_daq_widget = QThread()
         self.worker = SpectraDAQ(self.devicesMap, True)
-        self.worker.moveToThread(self.thr_daq_widget)
+        self.worker.moveToThread(self.thr_daq)
 
-        self.worker.signal_dataReady.connect(lambda devicesMap, useSum=True: self.m_sumSpectrometer.slot_on_spec_update(devicesMap, useSum))
-        self.worker.signal_dataReady.connect(lambda devicesMap, useSum=False: self.m_specWidget.slot_on_spec_update(devicesMap, useSum))
-        self.worker.finished.connect(self.thr_daq_widget.terminate)
+        self.worker.signal_dataReady.connect(lambda devicesMap, entry, useSum=True: self.m_sumSpectrometer.slot_on_spec_update(devicesMap, entry, useSum))
+        self.worker.signal_dataReady.connect(lambda devicesMap, entry, useSum=False: self.m_specWidget.slot_on_spec_update(devicesMap, entry, useSum))
+        self.worker.finished.connect(self.thr_daq.terminate)
 
-        self.thr_daq_widget.started.connect(self.worker.run)
-        self.cwidget.btn_get_spectrum.clicked.connect(self.thr_daq_widget.start)        
+        self.thr_daq.started.connect(self.worker.run)
+        self.daqwidget.btn_get_single_spectrum.clicked.connect(self.get_single_spectrum)     
+        self.daqwidget.btn_get_full_spectrum.clicked.connect(self.get_series_of_spectra)        
+   
+
+    def get_single_spectrum(self):
+        self.worker.nEntries = 1
+        self.thr_daq.start()
+
+    def get_series_of_spectra(self):
+        #dAcqTime = int(float(self.daqwidget.lne_full_daq_time.text())*1000/float(self.daqwidget.lne_single_daq_time.text()))
+        self.worker.nEntries = self.daqwidget.nFrames
+        self.thr_daq.start()
+
 
     def slot_dump_wave_done(self):
         self.daq_cont['dumping_wave'] = False 
@@ -143,10 +224,7 @@ class MainWindow(QMainWindow):
     def slot_dump_wave(self, samples_num, waves_num, channel_id):
         self.signal_dump_wave.emit(samples_num, waves_num, channel_id)
 
-    def slot_dump_spec(self, samples_num, waves_num):
-        self.signal_dump_spec.emit(samples_num, waves_num)
-
-    def initUI(self):
+    def setupUI(self):
         self.setWindowTitle("Multi-channel spectrometer")
         self.cwidget = ConnectionWidget()
 
@@ -154,6 +232,8 @@ class MainWindow(QMainWindow):
 
         self.boards_mgr = BoardsManager()
         self.devicesMap = self.cwidget.tbl_devices_list.devicesMap #!!!connect DeviceMap from table to main container
+        self.daqwidget = DaqWidget(self.devicesMap)
+
 
         self.oscope_widget = oscope.OscilloscopeW()
 
@@ -174,6 +254,7 @@ class MainWindow(QMainWindow):
         hlayout.addWidget(tabs_lyout)
         centerwidget = QWidget()
         win_lyout.addWidget(self.cwidget)
+        win_lyout.addWidget(self.daqwidget)
         win_lyout.addWidget(self.boards_mgr)
         centerwidget.setLayout(hlayout)
         self.setCentralWidget(centerwidget)
@@ -193,9 +274,9 @@ class MainWindow(QMainWindow):
         #if device was noe connected previously, add it to the devices table,
         #otherwise update information in devices table
         if not self.settings["ip"] in self.devicesMap:
-            self.cwidget.addFullData_on_Connected(self.board.check_connect(), uptime_seconds)
+            self.cwidget.setupData_onConnected(self.board.check_connect(), uptime_seconds, self.devicesMap, False)
         else:
-            self.cwidget.updFullData_on_Connected(self.board.check_connect(), uptime_seconds, self.devicesMap)
+            self.cwidget.setupData_onConnected(self.board.check_connect(), uptime_seconds, self.devicesMap, True)
 
         isConnected = self.board.check_connect()
         print("Connection:", isConnected)
