13e7b66653b89a3ff128319af369ed065b378759
Theywod <eruntsov.98@gmail.com>
2024-05-14 09:13:35 +0700

    change gitignore

--- oscilloscope/forms/waveform.py
+++ oscilloscope/forms/waveform.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'C:\Users\dkc1\Desktop\test-spec\test-spec\oscilloscope\forms/waveform.ui'
+# Form implementation generated from reading ui file '/home/biba/work/python/multichannel-spec/oscilloscope/forms/waveform.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.10
 #
